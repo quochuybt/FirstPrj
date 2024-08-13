d74da4c521c0cc9602d637a4a8c7ac0d1047d1af
quochuybt <quochuybt2005@gmail.com>
2024-08-13 18:18:32 +0700

    First Commit

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Hello Le Nguyen Quoc Huy!");
+        System.out.println("Welcome to the DHKTPM19ATT!");
+    }
+}
